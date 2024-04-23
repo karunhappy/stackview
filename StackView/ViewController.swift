fe7e41dfd90cba75206c2890143a286db2ed6431
karun_happy <karun.happy358@gmail.com>
2024-04-23 23:54:46 +0530

    UI layout

--- StackView/ViewController.swift
+++ StackView/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageviewTop: UIImageView!
+    @IBOutlet weak var shadowView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageviewTop.layer.cornerRadius = 20
+        imageviewTop.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        
+        shadowView.clipsToBounds = true
+        shadowView.layer.cornerRadius = 10
+        shadowView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        let shadowLayer = CALayer()
+        shadowLayer.frame = CGRect(x: 0, y: -1, width: shadowView.frame.width, height: 1)
+        shadowLayer.backgroundColor = UIColor.lightGray.cgColor
+        shadowLayer.shadowRadius = 3.0
+        shadowLayer.shadowOpacity = 1
+        shadowLayer.shadowOffset = CGSize(width: 0, height: -2.5)
+        
+        shadowView.layer.addSublayer(shadowLayer)
+        
     }
 
 
 }
 
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        } set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+}
